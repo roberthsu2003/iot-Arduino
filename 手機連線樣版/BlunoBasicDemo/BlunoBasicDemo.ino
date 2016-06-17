3e36cb4c633d053750174713f6c3c2e75cdf6e23
roberthsu <roberthsu@roberthsude-iMac.local>
2016-06-17 15:28:25 +0800

    initial

--- 手機連線樣版/BlunoBasicDemo/BlunoBasicDemo.ino
+++ 手機連線樣版/BlunoBasicDemo/BlunoBasicDemo.ino
@@ -1,7 +1,8 @@
-
-unsigned char iphoneData[10];
-int index = 0;
-char mobileMode = -1; // 0 is iphone,1 is android
+//手機使用變數begin
+  unsigned char mobileData[10];
+  int index = 0;
+  char mobileMode = -1; // 0 is iphone,1 is android
+//手機使用變數end
 
 void setup() {
     Serial.begin(115200);               //initial the Serial
@@ -19,32 +20,61 @@ void mobileControl(){
     {
         
         unsigned char incomingByte = Serial.read();
-        // set iphone mode or android mode
-        
+        // set iphone mode or android mode  
+
         if(incomingByte == 99){
           mobileMode = 0;
+        }else if(incomingByte == 98){          
+          mobileMode = 1;
         }
+        
+        
 
-        if(incomingByte == 100){ 
+        if(incomingByte == 100){           
             if (mobileMode == 0){          
-              iphoneControl(iphoneData,index);
-               memset(iphoneData,255,10);
+              mobileAction(mobileData,index);              
+            }else if(mobileMode == 1){
+              mobileAction(mobileData,index);             
             }
+             memset(mobileData,255,10);
             //set default          
-            index = 0;    
+            index = 0; 
+             
         }else{
-            iphoneData[index++] = incomingByte;
+            mobileData[index++] = incomingByte;
+           
         }
         
     }
 }
 
-void iphoneControl(unsigned char data[],int index){
-  
+void mobileAction(unsigned char data[],int index){
+  /*
     for(int i=0;i<index;i++){
         Serial.write(data[i]);
          delay(100);
        }
+       */
+       
+       //get feature
+       unsigned char feature = data[1];
+       if(feature == 0x02){
+          switchLight(data[2]);
+       }
+}
+
+
+
+void switchLight(unsigned char action){
+    if(action == 1){
+        digitalWrite(13,HIGH);
+        
+    }else if(action == 0){
+        digitalWrite(13,LOW);
+        
+    }
+
+    Serial.write(action);
 }
 
 
