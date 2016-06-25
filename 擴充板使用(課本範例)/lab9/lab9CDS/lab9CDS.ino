ede4a6fa2589b633b0f762cd29cc8677500c65a5
roberthsu <roberthsu@roberthsude-iMac.local>
2016-06-25 08:13:34 +0800

    initial

---
+++ 擴充板使用(課本範例)/lab9/lab9CDS/lab9CDS.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A4,INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(A4);
+  Serial.println(val);
+  delay(500);
+}
