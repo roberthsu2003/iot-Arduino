26e42dded524885115558d5d7ad63ef9184334f1
robert <roberthsu2003@gmail.com>
2016-12-05 10:36:18 +0800

    debounce

---
+++ 麵包板使用/Debounce/debounce.ino
@@ -0,0 +1,64 @@
+/*
+ Debounce
+??????pin?LOW?HIGH(e.g. ????????),????pin?LOW?HIGH??HIGH?ROW??debounce ??????,????????
+?????, ??????. 
+ */
+
+// ?????????????????
+
+const int buttonPin = 2;    // the number of the pushbutton pin
+const int ledPin = 13;      // the number of the LED pin
+
+// ?????????????
+int ledState = HIGH;         //????pin???
+int buttonState;             // ??input???
+int lastButtonState = LOW;   /???????????
+
+//?????????unsigned long????????????????????????int,?????int????????
+unsigned long lastDebounceTime = 0;  // ?output pin?????????
+unsigned long debounceDelay = 50;    // ?debounce???
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  // set initial LED state
+  digitalWrite(ledPin, ledState);
+}
+
+void loop() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH),  and you've waited
+  // long enough since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+      }
+    }
+  }
+
+  // set the LED:
+  digitalWrite(ledPin, ledState);
+
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
+}
