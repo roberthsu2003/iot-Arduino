5e158c2791fea1591b76449764a437a5233ec476
Robert Hsu <roberthsu2003@gmail.com>
2017-01-22 11:20:27 +0800

    Update Motor.h

--- 麵包板使用/自訂class/Motor.h
+++ 麵包板使用/自訂class/Motor.h
@@ -1,6 +1,12 @@
 #ifndef _MOTOR_H_
 #define _MOTOR_H_
 #include <arduino.h>
+
+#define SPEED0 0
+#define SPEED1 1
+#define SPEED2 2
+#define SPEED3 3
+#define SPEED4 4
 class Motor{
   protected:
     int _modePin1;
@@ -9,11 +15,14 @@ class Motor{
     int _speed;
     String _mode;
     void changeMotorState();
+    
    public:
+   static  String STOP;
+   static  String FOREWARD;
+   static  String BACKWARD;
     Motor(int pin1,int pin2,int speedPin);
     void setSpeed(int speed);//speed 0~4,default=0
     void setMode(String mode);//"stop","foreward","backward".default="stop"
 };
 #endif
-
 
