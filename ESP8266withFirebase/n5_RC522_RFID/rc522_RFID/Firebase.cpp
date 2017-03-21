68771f5d5f3dbd433e7f41e05cf53120abb960ee
robert <roberthsu2003@gmail.com>
2017-03-21 16:10:59 +0800

    modify

---
+++ ESP8266withFirebase/n5_RC522_RFID/rc522_RFID/Firebase.cpp
@@ -0,0 +1,128 @@
+#include "Firebase.h"
+
+
+Firebase::Firebase(const char* host) {
+  firebaseHost = host;
+}
+
+void Firebase::connectedToWifi(const char* ssid, const char* password) {
+
+  if (WiFi.status() != WL_CONNECTED) {
+    this -> ssid = ssid;
+    this -> password  = password;
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+    Serial.println("WiFi connected");
+    Serial.println("IP address:");
+    Serial.println(WiFi.localIP());
+  }
+}
+
+void Firebase::connectedToFirebase() {
+  if (WiFi.status() == WL_CONNECTED) {
+    if (!client.connect(firebaseHost, httpsPort)) {
+      Serial.println("connection failed");
+      return;
+    }
+    Serial.print("connecting to ");
+    Serial.println(firebaseHost);
+
+  } else {
+    return;
+  }
+}
+
+JsonObject* Firebase::getJSONData(String url) {
+  connectedToFirebase();
+
+  if (WiFi.status() == WL_CONNECTED && client.connected()) {
+    Serial.print("requesting URL:");
+    String urlRequest = "GET " + url + " HTTP/1.1\r\n" +
+                        "Host: " + firebaseHost + "\r\n" +
+                        "Connection: close\r\n\r\n";
+    Serial.println(urlRequest);
+    client.print(urlRequest);
+
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+      if (millis() - timeout > 5000) {
+        Serial.println(">>>>Client Timeout!");
+        client.stop();
+        return nullptr;
+      }
+    }
+
+    Serial.println("----------------data-------------");
+    String section = "header";
+
+    while (client.available()) {
+      String line = client.readStringUntil('\r');
+      if (section == "header") {
+        Serial.print(".");
+        //Serial.print(line);
+        if (line == "\n") {
+          section = "json";
+        }
+        continue;
+      }
+      Serial.print("line:");
+      Serial.println(line);
+      String result = line.substring(1);//前面有一個換行字元
+      Serial.print("result:");
+      Serial.println(result);
+      int size = result.length() + 1;//c字串內最後要加一個"/0"
+      char json[size];
+      result.toCharArray(json, size);
+      StaticJsonBuffer<200> jsonBuffer;
+      JsonObject& json_parsed = jsonBuffer.parseObject(json);
+      if (!json_parsed.success()) {
+        Serial.println();
+        Serial.println("parseObject() failed");
+        return nullptr;
+      } else {
+        Serial.println("closing connection");
+        //const char* d2State = json_parsed["D2"];
+        return &json_parsed;
+      }
+
+    }
+    Serial.println("closing connection");
+
+  } else {
+    return nullptr;
+  }
+}
+
+
+void Firebase::postJSONData(String tagID, String url) {
+  Serial.println(tagID);
+  if (WiFi.status() == WL_CONNECTED && client.connected()) {
+    Serial.print("requesting URL:");
+    String jsonString = "{\"id\":\"" + tagID + "\",\"status\":\"come in\"}";
+    //String jsonString = "{\"a1\":10}";
+    String urlRequest = "POST " + url + " HTTP/1.1\r\n" +
+                        "Host: " + firebaseHost + "\r\n" +
+                        "Content-Type: application/json\r\n" +
+                        "User-Agent: BuildFailureDetectorESP8266\r\n" +
+                        "Content-Length:" + jsonString.length() + "\r\n\r\n" +
+                        jsonString;
+
+    Serial.println(urlRequest);
+    client.print(urlRequest);
+
+    while (client.connected()) {
+
+      String line = client.readStringUntil('\n');
+      Serial.println(line);
+      if (line == "\r") {
+        Serial.println("headers received");
+        break;
+      }
+    }
+
+  }
+}
+
--- ESP8266withFirebase/n5_RC522_RFID/rc522_RFID/rc522_RFID.ino
+++ ESP8266withFirebase/n5_RC522_RFID/rc522_RFID/rc522_RFID.ino
@@ -1,8 +1,32 @@
+/**
+  Read a card using a mfrc522 reader on your SPI interface
+  Pin layout should be as follows (on Arduino Uno):
+  MOSI: Pin 11 / ICSP-4
+  MISO: Pin 12 / ICSP-1
+  SCK: Pin 13 / ISCP-3
+  SS: Pin 10
+  RST: Pin 9
+
+  Script is based on the script of Miguel Balboa.
+  New cardnumber is printed when card has changed. Only a dot is printed
+  if card is the same.
+
+  @version 0.1
+  @author Henri de Jong
+  @since 06-01-2013
+*/
+
 #include <SPI.h>
 #include <RFID.h>
+#include "Firebase.h"
+
 #define SS_PIN 15
-#define RST_PIN 0
+#define RST_PIN 16
+
+Firebase firebase("arduinofirebase-6d104.firebaseio.com");
 RFID rfid(SS_PIN, RST_PIN);
+unsigned long lastTime = millis();
+
 // Setup variables:
 int serNum0;
 int serNum1;
@@ -12,69 +36,92 @@ int serNum4;
 
 void setup()
 {
-  Serial.begin(115200);
+  Serial.begin(9600);
   SPI.begin();
   rfid.init();
+
+  firebase.connectedToWifi("robert_hsu_home", "0926656000");
+
 }
 
-void loop() {
-  if (rfid.isCard()) {
-    if (rfid.readCardSerial()) {
-      //Tag 序號
-      if (rfid.serNum[0] != serNum0
-          && rfid.serNum[1] != serNum1
-          && rfid.serNum[2] != serNum2
-          && rfid.serNum[3] != serNum3
-          && rfid.serNum[4] != serNum4
-         ) {
-        /* With a new cardnumber, show it. */ 
-        Serial.println(" "); 
-        Serial.println("Card found");
-        serNum0 = rfid.serNum[0];
-        serNum1 = rfid.serNum[1];
-        serNum2 = rfid.serNum[2];
-        serNum3 = rfid.serNum[3];
-        serNum4 = rfid.serNum[4];
-        //Serial.println(" ");
-        Serial.println("Cardnumber:");
-        Serial.print("Dec: ");
-        Serial.print(rfid.serNum[0], DEC);
-        Serial.print(", ");
-        Serial.print(rfid.serNum[1], DEC);
-        Serial.print(", ");
-        Serial.print(rfid.serNum[2], DEC);
-        Serial.print(", ");
-        Serial.print(rfid.serNum[3], DEC);
-        Serial.print(", ");
-        Serial.print(rfid.serNum[4], DEC);
-        Serial.println(" ");
-        Serial.print("Hex: ");
-        Serial.print(rfid.serNum[0], HEX);
-        Serial.print(", ");
-        Serial.print(rfid.serNum[1], HEX);
-        Serial.print(", ");
-        Serial.print(rfid.serNum[2], HEX);
-        Serial.print(", ");
-        Serial.print(rfid.serNum[3], HEX);
-        Serial.print(", ");
-        Serial.print(rfid.serNum[4], HEX);
-        Serial.println(" ");
-      } else {
-        /* If we have the same ID, just write a dot. */
-        Serial.print(".");
+void loop()
+{
+  if (millis() - lastTime > 1000) {
+    lastTime = millis();
+    if (rfid.isCard()) {
+      if (rfid.readCardSerial()) {
+        if (rfid.serNum[0] != serNum0
+            || rfid.serNum[1] != serNum1
+            || rfid.serNum[2] != serNum2
+            || rfid.serNum[3] != serNum3
+            || rfid.serNum[4] != serNum4
+           ) {
+          /* With a new cardnumber, show it. */
+          Serial.println(" ");
+          Serial.println("Card found");
+          serNum0 = rfid.serNum[0];
+          serNum1 = rfid.serNum[1];
+          serNum2 = rfid.serNum[2];
+          serNum3 = rfid.serNum[3];
+          serNum4 = rfid.serNum[4];
+
+          //Serial.println(" ");
+          Serial.println("Cardnumber:");
+          Serial.print("Dec: ");
+          Serial.print(rfid.serNum[0], DEC);
+          Serial.print(", ");
+          Serial.print(rfid.serNum[1], DEC);
+          Serial.print(", ");
+          Serial.print(rfid.serNum[2], DEC);
+          Serial.print(", ");
+          Serial.print(rfid.serNum[3], DEC);
+          Serial.print(", ");
+          Serial.print(rfid.serNum[4], DEC);
+          Serial.println(" ");
+
+          Serial.print("Hex: ");
+          Serial.print(rfid.serNum[0], HEX);
+          Serial.print(", ");
+          Serial.print(rfid.serNum[1], HEX);
+          Serial.print(", ");
+          Serial.print(rfid.serNum[2], HEX);
+          Serial.print(", ");
+          Serial.print(rfid.serNum[3], HEX);
+          Serial.print(", ");
+          Serial.print(rfid.serNum[4], HEX);
+          Serial.println(" ");
+          //run firebase
+          firebase.connectedToFirebase();
+          String id = "";
+          for(int i=0;i<4;i++){
+            char charVal[2];
+            sprintf(charVal,"%2X",rfid.serNum[i]);
+            id += (String(charVal));
+            if(i!=3){
+              id += "-";
+            }
+          }
+          firebase.postJSONData(id,"/rfid1.json");
+        } else {
+          /* If we have the same ID, just write a dot. */
+          Serial.print(".");
+        }
+
+
       }
-    }
-  }else{
-    //card move out reader
-    //reset others
-    
-      Serial.println("no card");
+
+    } else {
       serNum0 = 0;
       serNum1 = 0;
       serNum2 = 0;
       serNum3 = 0;
       serNum4 = 0;
+      Serial.println("no card or times error");
+      
+    }
+    rfid.halt();
+
+    
   }
-  rfid.halt();
-  delay(200) ; // delay 200 ms
 }
+
