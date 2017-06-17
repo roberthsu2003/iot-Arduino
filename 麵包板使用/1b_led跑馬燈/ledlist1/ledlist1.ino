731b73a8e50f8492d08777fcc00de69072089560
Robert Hsu <roberthsu2003@gmail.com>
2017-06-17 14:03:59 +0800

    modify

--- 麵包板使用/1b_led跑馬燈/ledlist1/ledlist1.ino
+++ 麵包板使用/1b_led跑馬燈/ledlist1/ledlist1.ino
@@ -1,25 +1,55 @@
 const byte LED1 = 8;
 const byte LED2 = 9;
 const byte LED3 = 10;
+const byte LED4 = 11;
+const byte LED5 = 12;
+
+byte leds[] = {LED1, LED2, LED3, LED4, LED5};
 
 void setup() {
-  pinMode(LED1,OUTPUT);
-  pinMode(LED2,OUTPUT);
-  pinMode(LED3,OUTPUT);
+  // put your setup code here, to run once:
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
+  pinMode(LED5, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED1,HIGH);
-  digitalWrite(LED2,LOW);
-  digitalWrite(LED3,LOW);
-  delay(500);
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED2,HIGH);
-  digitalWrite(LED3,LOW);
-  delay(500);
-  digitalWrite(LED1,LOW);
-  digitalWrite(LED2,LOW);
-  digitalWrite(LED3,HIGH);
-  delay(500);
+  
+    digitalWrite(LED1,HIGH);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    digitalWrite(LED4,LOW);
+    digitalWrite(LED5,LOW);
+    delay(200);
+
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,HIGH);
+    digitalWrite(LED3,LOW);
+    digitalWrite(LED4,LOW);
+    digitalWrite(LED5,LOW);
+    delay(200);
+
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,HIGH);
+    digitalWrite(LED4,LOW);
+    digitalWrite(LED5,LOW);
+    delay(200);
+
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    digitalWrite(LED4,HIGH);
+    digitalWrite(LED5,LOW);
+    delay(200);
+
+    digitalWrite(LED1,LOW);
+    digitalWrite(LED2,LOW);
+    digitalWrite(LED3,LOW);
+    digitalWrite(LED4,LOW);
+    digitalWrite(LED5,HIGH);
+    delay(200);
 }
