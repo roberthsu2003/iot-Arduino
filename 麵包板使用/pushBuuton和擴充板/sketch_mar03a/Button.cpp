74d0985507ae367216054ab68fa92fa3fab3baaa
Robert Hsu <roberthsu2003@gmail.com>
2017-12-23 14:29:16 +0800

    modify

--- 麵包板使用/pushBuuton和擴充板/sketch_mar03a/Button.cpp
+++ 麵包板使用/pushBuuton和擴充板/sketch_mar03a/Button.cpp
@@ -1,42 +1,37 @@
+//Button.cpp
 #include "Button.h"
 
-Button::Button(byte pinNum,int mode){
-    pinNumber = pinNum;
-    pinMode(pinNumber,mode);
-    if(mode == INPUT_PULLUP){
-        lastBtnState = true;
-    }else if(mode == INPUT){
-        lastBtnState = false;
-    }
+Button::Button(int buttonPin, CallBackType c) {
+  _buttonPin = buttonPin;
+  pinMode(_buttonPin, INPUT_PULLUP);
+  _buttonState = digitalRead(_buttonPin);
+  _callBack = c;
+}
+
+int Button::getButtonPin() {
+  return _buttonPin;
+}
+
+void Button::buttonSensor() {
+  //取得按下放開的次數
+  bool lastButtonState = digitalRead(_buttonPin);
+  if (_buttonState != lastButtonState) {
     
-}
-
-
-
-
-void Button::buttonRunning(){
-    bool newBtnState = digitalRead(pinNumber);
-    if(newBtnState != lastBtnState){
-        lastTime = millis();
-        lastBtnState = newBtnState;
-        isReadyGet = true;
-    }
-    unsigned long newTime = millis();
-    if((newTime-lastTime) >= 30 && isReadyGet == true){
-        clickNum++;
-        isReadyGet = false;
-    }
-    Serial.println(clickNum);
-    clickTimes = clickNum/2;
-    byte buttonTime = clickTimes % recycleCount;
-    callBackArray[buttonTime]();
-    delay(10);    
-}
-
-
-void Button::setup(callBackType *a,int num){
-    callBackArray = a;
-    recycleCount = num;
+    //使用delay才不會bounce
+    delay(20);     
+      if (lastButtonState == digitalRead(_buttonPin)) {
+        pressTime++;
+        _callBack(pressTime);
+        
+      }
+      _buttonState = digitalRead(_buttonPin);
     
+
+  }
 }
+
+void Button::running() {
+  buttonSensor();
+}
+
 
