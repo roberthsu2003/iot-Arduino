241f61e398fe01ae71064260948380ea242d2eea
Robert Hsu <roberthsu2003@gmail.com>
2017-12-23 14:28:45 +0800

    modify

--- 麵包板使用/pushBuuton和擴充板/sketch_mar03a/Button.h
+++ 麵包板使用/pushBuuton和擴充板/sketch_mar03a/Button.h
@@ -1,27 +1,24 @@
-#include "arduino.h"
-using callBackType = void(*)(void);
+//Button.h
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+#include <arduino.h>
 
-class Button{
-public:
-    //declare Field
+using CallBackType = void(*)(long int);
+
+class Button {
+  private:
+    int _buttonPin;
+    CallBackType _callBack;
+    bool _buttonState;
+    long int pressTime = 0;
     
-    byte pinNumber;
-    bool lastBtnState;
-    bool isReadyGet = false;
-    unsigned long lastTime;
-    unsigned int clickNum = 0;
-    unsigned int clickTimes = 0;
-    byte recycleCount = 2;
-    callBackType *callBackArray;
-    
-    //declare constructure
-    //建構式
-    
-    Button(byte pinNum,int mode);
-    //declare method
-    void buttonRunning();
-    byte getButtonTime();
-    void setup(callBackType *a,int num);
+  public:
+    Button(int buttonPin, CallBackType c);
+    int getButtonPin();
+    void running();
+    void buttonSensor();
 };
+
+#endif
 
 
