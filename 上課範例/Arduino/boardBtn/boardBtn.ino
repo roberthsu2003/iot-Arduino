4d6e867f9d1df36fb8ef1c2c4c2d09902fe859ff
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-03 15:18:37 +0800

    modify

--- 上課範例/Arduino/boardBtn/boardBtn.ino
+++ 上課範例/Arduino/boardBtn/boardBtn.ino
@@ -1,18 +1,27 @@
+unsigned long n1StartPoint;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(A1,INPUT_PULLUP);
+  pinMode(A1, INPUT_PULLUP);
   pinMode(13, OUTPUT);
+
+  n1StartPoint = millis();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int buttonState = digitalRead(A1);
-  if(buttonState == LOW){
-    digitalWrite(13, HIGH);
-  }else{
-    digitalWrite(13, LOW);
+  unsigned long n1CurrentPoint = millis();
+  if (n1CurrentPoint - n1StartPoint >= 100) {
+    int buttonState = digitalRead(A1);
+    if (buttonState == LOW) {
+      digitalWrite(13, HIGH);
+    } else {
+      digitalWrite(13, LOW);
+    }
+    Serial.println(buttonState);
+    n1StartPoint = n1CurrentPoint;
   }
-  Serial.println(buttonState);
-  delay(100);
+
+
 }
