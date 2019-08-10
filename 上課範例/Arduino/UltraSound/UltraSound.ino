db0d6246fca25d79dc3455c6a5ac1dbf84e79ec5
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-10 16:18:16 +0800

    modify

--- 上課範例/Arduino/UltraSound/UltraSound.ino
+++ 上課範例/Arduino/UltraSound/UltraSound.ino
@@ -1,11 +1,10 @@
+#include <Ultrasonic.h>
 #define RELAY 8
 #define TRIGER 3
 #define ECHO 4
-
+Ultrasonic ultrasonic(TRIGER, ECHO);
 boolean lightState = false;
-
 void setup() {
-  pinMode(A4,INPUT_PULLUP);
   pinMode(RELAY,OUTPUT);
   Serial.begin(115200);
 
@@ -13,20 +12,19 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int cdsValue = analogRead(A4);
-  Serial.println(cdsValue);
-  if(cdsValue > 200){
+  long microsec = ultrasonic.timing();
+  float cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  if(cmMsec <= 10){
     if (lightState == false){
       digitalWrite(RELAY,HIGH);
       lightState = true;
     }
-    
   }else{
-    if(lightState == true){
+     if(lightState == true){
        digitalWrite(RELAY,LOW);
       lightState = false;
     }
   }
-  
+  Serial.println(cmMsec);
   delay(100);
 }
