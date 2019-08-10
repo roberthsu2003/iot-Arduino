9affac834e771f3148f5aa53653f9800bc5649f5
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-10 14:58:46 +0800

    modify

--- 上課範例/Arduino/cds/cds.ino
+++ 上課範例/Arduino/cds/cds.ino
@@ -1,5 +1,9 @@
+#define RELAY 4
+boolean lightState = false;
+
 void setup() {
   pinMode(A4,INPUT_PULLUP);
+  pinMode(RELAY,OUTPUT);
   Serial.begin(115200);
 
 }
@@ -8,5 +12,18 @@ void loop() {
   // put your main code here, to run repeatedly:
   int cdsValue = analogRead(A4);
   Serial.println(cdsValue);
-  delay(500);
+  if(cdsValue > 200){
+    if (lightState == false){
+      digitalWrite(RELAY,HIGH);
+      lightState = true;
+    }
+    
+  }else{
+    if(lightState == true){
+       digitalWrite(RELAY,LOW);
+      lightState = false;
+    }
+  }
+  
+  delay(100);
 }
