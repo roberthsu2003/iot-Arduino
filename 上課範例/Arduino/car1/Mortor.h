63309b6241e6bd87c929c09cb0a7c9452661e18b
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-17 14:44:43 +0800

    modify

--- 上課範例/Arduino/car1/Mortor.h
+++ 上課範例/Arduino/car1/Mortor.h
@@ -1,12 +1,19 @@
 #ifndef __MORTOR__
 #define __MORTOR__
 
+#include <arduino.h>
+
+typedef enum {
+  STOP,FOREWARD, BACKWARD
+} MortorMode;
+
 class Mortor{
   private:
     int _modePin1;
     int _modePin2;
     int _speedPin;
     int _speed = 0;
+    MortorMode _mode = STOP;//stop, foreward, backward
     
   public:
     Mortor(int modePin1, int modePin2, int speedPin);
--- 上課範例/Arduino/car1/car1.ino
+++ 上課範例/Arduino/car1/car1.ino
@@ -1,5 +1,7 @@
 #include "Mortor.h"
 
+Mortor leftMortor(10, 9 , 6);
+Mortor rightMortor(12, 11, 5);
 void setup() {
   // put your setup code here, to run once:
 
