314c6232ba31df67324ec3e7d1bce7b0b8ba182e
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-24 10:23:32 +0800

    modify

--- 上課範例/Arduino/car1/car1.ino
+++ 上課範例/Arduino/car1/car1.ino
@@ -1,15 +1,21 @@
 #include "Mortor.h"
+#include <Ultrasonic.h>
 
 Mortor leftMortor(10, 9 , 6);
 Mortor rightMortor(12, 11, 5);
 
+Ultrasonic frontUltra(3,4);
 void setup() {
   // put your setup code here, to run once:
-  
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  rightMortor.setMode(FOREWARD);
-  rightMortor.setSpeed(10);
+  rightMortor.setMode(STOP);
+  rightMortor.setSpeed(0);
+  long microsec = frontUltra.timing();
+  int frontCM = frontUltra.convert(microsec,Ultrasonic::CM);
+  Serial.println(frontCM);
+  delay(100);
 }
