01fb5f56ccf3e127e4d1e4e0a03ace2243edc849
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-24 14:27:08 +0800

    modify

--- 上課範例/Arduino/testEsp8266/testEsp8266.ino
+++ 上課範例/Arduino/testEsp8266/testEsp8266.ino
@@ -3,7 +3,7 @@
 void setup() {
   Serial.begin(115200);
   Serial.println();
-  WiFi.begin("RobertIphone", "0926656000");
+  WiFi.begin("robert_hsu", "1234567890");
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
   {
