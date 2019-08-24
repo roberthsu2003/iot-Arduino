fc31a730df7eb35853b7f6a7048423a610669b4b
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-24 13:49:38 +0800

    modify

--- 上課範例/Arduino/testEsp8266/testEsp8266.ino
+++ 上課範例/Arduino/testEsp8266/testEsp8266.ino
@@ -1,10 +1,20 @@
+#include <ESP8266WiFi.h>
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
+  Serial.println();
+  WiFi.begin("RobertIphone", "0926656000");
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Hello!World");
-  delay(1000);
+
 }
