971f74a519dfba94e3e3ad7a616c4dd8a4fea366
roberthsu2004 <roberthsu2004@gmail.com>
2019-08-31 11:05:24 +0800

    modify

---
+++ 上課範例/Arduino/arduinoWiFiTest/arduinoWiFiTest.ino
@@ -0,0 +1,19 @@
+#include <ESP8266WiFi.h>
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  WiFi.begin("robert_hsu", "1234567890");
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+void loop() {}
