44e24124bc68ad4e7f65dcb1ba157fccebd6268d
roberthsu2004 <roberthsu2004@gmail.com>
2019-09-07 10:18:59 +0800

    modify

---
+++ 上課範例/Arduino/relay/relay.ino
@@ -0,0 +1,11 @@
+#define IN1 D4
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(IN1,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(IN1,HIGH);
+}
