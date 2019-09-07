02223216684909c37cad357d9f08771ba7b7f673
roberthsu2004 <roberthsu2004@gmail.com>
2019-09-07 12:23:32 +0800

    modify

--- 上課範例/Arduino/relay/relay.ino
+++ 上課範例/Arduino/relay/relay.ino
@@ -1,18 +1,31 @@
 #define IN1 D4
 #define BTN D1
 
+bool btnState;
+unsigned long count = 0;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(IN1,OUTPUT);
   pinMode(BTN,INPUT);
+  btnState = digitalRead(BTN);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  bool btnState = digitalRead(BTN);
-  if(btnState == true){
-    digitalWrite(IN1,HIGH);
-  }else{
-    digitalWrite(IN1,LOW);
+  bool currentState = digitalRead(BTN);
+  if(currentState != btnState){
+    count += 1;
+    btnState = currentState;
   }
+
+  switch (count % 4){
+    case 0:
+      digitalWrite(IN1,HIGH);
+      break;
+    case 2:
+      digitalWrite(IN1,LOW);
+      break;
+  }
+  
 }
