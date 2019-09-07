c33693a3f46f5209cbea8bbd916f89fa84920f0a
roberthsu2004 <roberthsu2004@gmail.com>
2019-09-07 11:40:15 +0800

    modify

--- 上課範例/Arduino/relay/relay.ino
+++ 上課範例/Arduino/relay/relay.ino
@@ -1,11 +1,18 @@
 #define IN1 D4
+#define BTN D1
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(IN1,OUTPUT);
+  pinMode(BTN,INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(IN1,HIGH);
+  bool btnState = digitalRead(BTN);
+  if(btnState == true){
+    digitalWrite(IN1,HIGH);
+  }else{
+    digitalWrite(IN1,LOW);
+  }
 }
