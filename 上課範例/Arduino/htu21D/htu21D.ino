4c456dfc7b2e4135d40a1f15735451cdae27cf91
roberthsu2004 <roberthsu2004@gmail.com>
2019-09-21 12:11:23 +0800

    modify

--- 上課範例/Arduino/htu21D/htu21D.ino
+++ 上課範例/Arduino/htu21D/htu21D.ino
@@ -1,16 +1,22 @@
 #include <Wire.h>
 #include "SparkFunHTU21D.h"
+#include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
+
 
 //Create an instance of the object
 HTU21D myHumidity;
+FirebaseData firebaseData;
+
+ 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("HTU21D Example!");  
   myHumidity.begin();
-
+  
+  //wifi
   WiFi.begin("robert_hsu", "1234567890");
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED)
@@ -21,6 +27,12 @@ void setup() {
   Serial.println();
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());
+
+  //firebase
+  Firebase.begin("arduinofirebase-6d104.firebaseio.com", "z5lPWwjZLZuNNcUEelbJdiNaIvnR2Zfq49BuQBAa");
+  Firebase.reconnectWiFi(true);
+  Firebase.setMaxRetry(firebaseData, 3);
+  Firebase.setMaxErrorQueue(firebaseData, 30);
 }
 
 void loop() {
@@ -37,5 +49,16 @@ void loop() {
   Serial.print("%");
 
   Serial.println();
+  if (Firebase.setDouble(firebaseData,"/home/humidity",humd) == true){
+    Serial.println("humidity success");
+  }else{
+    Serial.println("humidity false");
+  }
+
+  if (Firebase.setDouble(firebaseData,"/home/temp",temp) == true){
+    Serial.println("temperature success");
+  }else{
+    Serial.println("temperature false");
+  }
   delay(1000);
 }
