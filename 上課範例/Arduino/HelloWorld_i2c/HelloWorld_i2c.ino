9ee090f98052b9892eba6b285a966de2953c6bb6
roberthsu2004 <roberthsu2004@gmail.com>
2019-09-28 10:45:15 +0800

    modify

--- 上課範例/Arduino/HelloWorld_i2c/HelloWorld_i2c.ino
+++ 上課範例/Arduino/HelloWorld_i2c/HelloWorld_i2c.ino
@@ -5,7 +5,7 @@
 
 #define BACKLIGHT_PIN     13
 
-LiquidCrystal_I2C lcd(0x38);  // Set the LCD I2C address
+LiquidCrystal_I2C lcd(0x27);  // Set the LCD I2C address
 
 //LiquidCrystal_I2C lcd(0x38, BACKLIGHT_PIN, POSITIVE);  // Set the LCD I2C address
 
