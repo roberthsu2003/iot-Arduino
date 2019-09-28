d9287278265406ad44db121a1bd2228101c876d3
roberthsu2004 <roberthsu2004@gmail.com>
2019-09-28 15:43:27 +0800

    modify

--- 上課範例/Arduino/lcdControl/lcdControl.ino
+++ 上課範例/Arduino/lcdControl/lcdControl.ino
@@ -2,9 +2,13 @@
 #include <LiquidCrystal_I2C.h>
 #include "FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
+#include "Timer.h"
+
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // 設定 LCD I2C 位址
 FirebaseData firebaseData;
 
+//timer
+Timer t;
 void setup() {
   Serial.begin(115200);  // 用於手動輸入文字
   lcd.begin(16, 2);      // 初始化 LCD，一行 16 的字元，共 2 行，預設開啟背光
@@ -25,9 +29,35 @@ void setup() {
   lcd.setCursor(0, 1); // 設定游標位置在第二行行首
   lcd.print("Robert_HSU");
 
+   //wifi
+
+  WiFi.begin("0gm4", "2773524311");
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
+
+  //firebase
+  Firebase.begin("arduinofirebase-6d104.firebaseio.com", "z5lPWwjZLZuNNcUEelbJdiNaIvnR2Zfq49BuQBAa");
+  Firebase.reconnectWiFi(true);
+  Firebase.setMaxRetry(firebaseData, 3);
+  Firebase.setMaxErrorQueue(firebaseData, 30);
+
+  //timer
+  t.every(500,getFirebaseData);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  t.update();
+}
 
+void getFirebaseData(){
+  Serial.println("getData");
 }
