733b4e1016bb5749522e4d81752bff111b1c121e
roberthsu2004 <roberthsu2004@gmail.com>
2019-09-28 10:23:23 +0800

    modify

---
+++ 上課範例/Arduino/LCD_rfid/LCD_rfid.ino
@@ -0,0 +1,10 @@
+//https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads/
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
