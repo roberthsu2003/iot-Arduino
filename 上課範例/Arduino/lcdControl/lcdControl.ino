1c5e1338c4c577d7e9184659aa07347b75394dc0
roberthsu2004 <roberthsu2004@gmail.com>
2019-09-28 15:30:46 +0800

    modify

---
+++ 上課範例/Arduino/lcdControl/lcdControl.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>  // Arduino IDE 內建
+#include <LiquidCrystal_I2C.h>
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // 設定 LCD I2C 位址
+FirebaseData firebaseData;
+
+void setup() {
+  Serial.begin(115200);  // 用於手動輸入文字
+  lcd.begin(16, 2);      // 初始化 LCD，一行 16 的字元，共 2 行，預設開啟背光
+
+  // 閃爍三次
+  for (int i = 0; i < 3; i++) {
+    lcd.backlight(); // 開啟背光
+    delay(250);
+    lcd.noBacklight(); // 關閉背光
+    delay(250);
+  }
+  lcd.backlight();
+
+  // 輸出初始化文字
+  lcd.setCursor(0, 0); // 設定游標位置在第一行行首
+  lcd.print("Hello, world!");
+  delay(1000);
+  lcd.setCursor(0, 1); // 設定游標位置在第二行行首
+  lcd.print("Robert_HSU");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
