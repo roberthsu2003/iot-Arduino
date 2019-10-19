abd9ff7b7a952408454d913b23b591da53259612
roberthsu2004 <roberthsu2004@gmail.com>
2019-10-19 10:29:42 +0800

    modify

--- 上課範例/Arduino/ultrasonic/ultrasonic.ino
+++ 上課範例/Arduino/ultrasonic/ultrasonic.ino
@@ -1,11 +1,17 @@
 #include "Ultrasonic.h"
 
+Ultrasonic front(3,4);
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+ long millsecon = front.timing();
+ float frontDistance = front.convert(millsecon, Ultrasonic::CM);
+ Serial.println(frontDistance);
+ delay(1000);
+ 
 }
