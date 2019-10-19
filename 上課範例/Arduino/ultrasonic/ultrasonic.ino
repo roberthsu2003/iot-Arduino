3b5490a39a84f5d1e52e4091419b7fc98d1a5288
roberthsu2004 <roberthsu2004@gmail.com>
2019-10-19 11:10:34 +0800

    modify

--- 上課範例/Arduino/ultrasonic/ultrasonic.ino
+++ 上課範例/Arduino/ultrasonic/ultrasonic.ino
@@ -1,8 +1,8 @@
 #include "Ultrasonic.h"
 
-Ultrasonic front(3,4);
-Ultrasonic left(A1,A2);
-ULtrasonic right(A3,A4);
+Ultrasonic front(3, 4);
+Ultrasonic right(A1, A2);
+Ultrasonic left(A3, A4);
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,10 +10,27 @@ void setup() {
 }
 
 void loop() {
+ultrasonicSensor();
+
+}
+
+void ultrasonicSensor() {
   // put your main code here, to run repeatedly:
- long millsecon = front.timing();
- float frontDistance = front.convert(millsecon, Ultrasonic::CM);
- Serial.println(frontDistance);
- delay(1000);
- 
+  long frontMillsecon = front.timing();
+  float frontDistance = front.convert(frontMillsecon, Ultrasonic::CM);
+  Serial.print("Front-->");
+  Serial.println(frontDistance);
+  //left
+  long leftMillsecon = left.timing();
+  float leftDistance = front.convert(leftMillsecon, Ultrasonic::CM);
+  Serial.print("Left-->");
+  Serial.println(leftDistance);
+
+  //right
+  long rightMillsecon = right.timing();
+  float rightDistance = right.convert(rightMillsecon, Ultrasonic::CM);
+  Serial.print("Right-->");
+  Serial.println(rightDistance);
+
+  delay(1000);
 }
